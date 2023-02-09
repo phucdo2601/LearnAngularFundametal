44720695813091e14b1342f34059a07c47fa9ff8
Phuc Do <ngocphucdo2601@gmail.com>
2023-02-09 11:56:23 +0700

    First learn basic DaBinding ngIf b01

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name: string = "Phuc Do";
+  public age: number = 15;
 
   constructor() { }
 
