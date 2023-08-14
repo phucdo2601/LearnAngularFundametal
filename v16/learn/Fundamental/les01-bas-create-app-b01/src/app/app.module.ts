e2553b34c06a5a5554aa2947fa9b69e430a1e158
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-08-14 19:01:31 +0700

    [learn-angular16] 03-Learn Basic component b01

--- v16/learn/Fundamental/les01-bas-create-app-b01/src/app/app.module.ts
+++ v16/learn/Fundamental/les01-bas-create-app-b01/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent
+    StudentComponent,
+    TeacherComponent
   ],
   imports: [
     BrowserModule,
--- v16/learn/Fundamental/les01-bas-create-app-b01/src/app/student/student.component.ts
+++ v16/learn/Fundamental/les01-bas-create-app-b01/src/app/student/student.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-student',
-  templateUrl: './student.component.html',
+  // templateUrl: './student.component.html',
+  template: '<h1>Hello World</h1>',
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent {
